40e9d8296796294871462647c7264aaffaca264a
Jaeyoung Lee <ljae1129@gmail.com>
2019-11-01 22:59:20 +0900

    Add masquerade slot

--- slot_machine/slot.py
+++ slot_machine/slot.py
@@ -96,7 +96,7 @@ class game_platinum:
         r_full = 0
         for i in range(5):
             probs = self.get_weighted_probability(game)
-            if game.count('R') >= 9:
+            if game.count('R') >= 7:
                 r_full = 1
             col_type = self.get_platinum_coltype(i)
             game.extend(self.get_platinum_symbol(col_type, probs, r_full))
@@ -241,7 +241,7 @@ class game_monster:
     
     def get_monster_weighted_probability(self, game):
         weight = [26, 24, 28, 28, 25, 19, 15, 13, 9, 3]
-        switch = [0] * 11
+        switch = [0] * 10
         unique_list = list(set(game))
         for n, s in enumerate(self.ms_labels[:10]):
             if s in unique_list:
@@ -303,3 +303,141 @@ class game_monster:
 
 game_monster(2000).play_monster()
 
+class game_masque:
+    """
+    monster 슬롯 구현
+    """
+    
+    def __init__(self, n, bpl):
+        self.n = n
+        self.bpl = bpl
+        self.paylines = [[0, 3, 6, 9, 12],
+                         [1, 4, 7, 10, 13],
+                         [2, 5, 8, 11, 14],
+                         [0, 4, 8, 10, 12],
+                         [2, 4, 6, 10, 14],
+                         [0, 3, 7, 11, 14],
+                         [2, 5, 7, 9, 12],
+                         [0, 4, 7, 10, 12],
+                         [1, 5, 8, 11, 13],
+                         [1, 3, 6, 9, 13],
+                         [2, 4, 7, 10, 14],
+                         [0, 4, 6, 10, 12],
+                         [1, 5, 7, 11, 13],
+                         [1, 3, 7, 9, 13],
+                         [2, 4, 8, 10, 14],
+                         [1, 4, 6, 10, 13],
+                         [1, 4, 8, 10, 13],
+                         [0, 5, 6, 11, 12],
+                         [2, 3, 8, 9, 14],
+                         [0, 3, 8, 9, 12],
+                         [2, 5, 6, 11, 14],
+                         [0, 5, 7, 9, 14],
+                         [2, 3, 7, 11, 12],
+                         [0, 5, 8, 11, 12],
+                         [2, 3, 6, 9, 14],
+                         [1, 3, 8, 10, 12],
+                         [1, 5, 6, 10, 14],
+                         [0, 3, 7, 9, 12],
+                         [2, 5, 7, 11, 14],
+                         [0, 5, 7, 11, 12]]
+        self.mq_labels = ['J', 'Q', 'K', 'A', 'F', 'H', 'Ms', 'St', 'Mn', 'Wm', 'W', 'Sc']
+        self.mq_paytable = pd.read_csv('../data/slot_machine/masque_csv/mq_paytable.csv')
+        self.mq_all = pd.read_csv('../data/slot_machine/masque_csv/mq_all.csv')
+        self.col_types = ['3', '2-1', '2-2', '2-3', '1']
+
+    def get_masque_coltypes(self, i):
+        if i==0:
+            return np.random.choice(self.col_types, 1, p=[0.596, 0.064, 0.052, 0.069, 0.219]).tolist()[0]
+        if i==1:
+            return np.random.choice(self.col_types, 1, p=[0.485, 0.154, 0.038, 0.16, 0.163]).tolist()[0]
+        if i==2:
+            return np.random.choice(self.col_types, 1, p=[0.461, 0.162, 0.076, 0.157, 0.144]).tolist()[0]
+        if i==3:
+            return np.random.choice(self.col_types, 1, p=[0.51, 0.141, 0.062, 0.132, 0.155]).tolist()[0]
+        if i==4:
+            return np.random.choice(self.col_types, 1, p=[0.693, 0.084, 0.085, 0.09, 0.048]).tolist()[0]
+        
+    def get_masque_scatter(self):
+        return np.random.choice([0, 1, 2, 3, 4, 5], 1, p=[0.5483, 0.3295, 0.103, 0.017, 0.002, 0.0002]).tolist()[0]
+    
+    def get_masque_symbol(self, col_type, probs):
+        n_labels = self.ms_labels[:11]
+        n_probs = [i / sum(probs) for i in probs]
+
+        if col_type == '3':
+            reel = np.random.choice(n_labels, 3, replace=False, p=n_probs).tolist()
+        elif col_type == '2-1':
+            reel = np.random.choice(n_labels, 2, replace=False, p=n_probs).tolist()
+            reel.insert(1, reel[0])
+        elif col_type == '2-2':
+            reel = np.random.choice(n_labels, 2, replace=False, p=n_probs).tolist()
+            reel.insert(2, reel[0])
+        elif col_type == '2-3':
+            reel = np.random.choice(n_labels, 2, replace=False, p=n_probs).tolist()
+            reel.reverse()
+            reel.insert(2, reel[1])
+        elif col_type == '1':
+            reel = np.random.choice(n_labels, 1, replace=False, p=n_probs).tolist() * 3
+        return reel
+    
+    def get_masque_weighted_probability(self, game):
+        weight = [27, 24, 26, 24, 21, 18, 17, 15, 8, 9, 3]
+        switch = [0] * 11
+        unique_list = list(set(game))
+        for n, s in enumerate(self.ms_labels[:10]):
+            if s in unique_list:
+                switch[n] = 2
+            if game.count(s) >= 6:
+                switch[n] = 4
+        probs = [w*(switch[n]+1) for n, w in enumerate(weight)]
+        return probs
+    
+    def spin_masque(self):
+        game = []
+        scatter_num = self.get_masque_scatter()
+        scatter_position = np.random.choice([0, 1, 2, 3, 4], scatter_num, replace=False, p=[0.185, 0.171, 0.194, 0.234, 0.216]).tolist()
+        for i in range(5):
+            probs = self.get_masque_weighted_probability(game)
+            col_type = self.get_masque_coltypes(i)
+            game.extend(self.get_masque_symbol(col_type, probs))
+        for sp in scatter_position:
+            game[np.random.randint(sp*3, sp*3+3)] = 'S'
+
+    def get_symbol(self, lst):
+        if len(set(lst)) < 2:
+            return lst[0]
+        else:
+            for x in list(set(lst)):
+                if x != 'W':
+                    return x
+                else:
+                    continue
+    
+    def cal_masque_payline(self, game, bpl):
+        pay = 0
+        for payline in self.paylines:
+            tmp = []
+            for i in range(5):
+                if i == 4:
+                    tmp.append(game[payline[i]])
+                    break
+                if i == 0 and game[payline[i]] == 'W':
+                    tmp.append(game[payline[i]])
+                elif game[payline[i]] != game[payline[i+1]] and game[payline[i+1]] != 'W':
+                    tmp.append(game[payline[i]])
+                    break
+                else:
+                    tmp.append(game[payline[i]])
+            if len(tmp) <= 2:
+                tmp = []
+            else:
+                sym = self.get_symbol(tmp)
+                pay += self.ms_paytable.loc[len(tmp)-3, sym] * bpl
+        return pay
+
+    def play_masque(self):
+        
+        game = self.spin_masque()
+        pay = self.cal_monster_payline(game, self.bpl)
+        return pay
