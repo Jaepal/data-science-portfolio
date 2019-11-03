45a824e3cbb11dd9f7e5fc3af334d413011d4c66
Jaeyoung Lee <ljae1129@gmail.com>
2019-11-03 23:12:37 +0900

    Add monster masque slot

--- slot_machine/image_rename.py
+++ slot_machine/image_rename.py
@@ -1,10 +1,26 @@
 from os import rename, listdir 
 
 files_platinum = listdir('.')
+files_monster = listdir('.')
+files_masque = listdir('.')
 
 count = 0 
 for name in files_platinum:
    
     new_name = name.replace(name, "platinum_%d.jpg" %count)
+    rename(name,new_name)
+    count += 1
+
+count = 0
+for name in files_monster:
+   
+    new_name = name.replace(name, "monster_%d.jpg" %count)
+    rename(name,new_name)
+    count += 1
+
+count = 0
+for name in files_masque:
+   
+    new_name = name.replace(name, "masque_%d.jpg" %count)
     rename(name,new_name)
     count += 1
